1ffd63bc6df61012c488d212d1bf19887afc9426
Oleg Konovalov V <konovalov_o@vk.com>
2025-03-22 18:06:08 +0300

    Home portfolio card

---
+++ template-parts/components/home.card-portfolio.php
@@ -0,0 +1,22 @@
+<?php
+    /**
+     * @var array $args - массив переданных значений поста;
+     * */
+    wp_enqueue_style('didistudio-art-component-home-card-portfolio');
+?>
+
+<?php if (!empty($args)): ?>
+    <article class="home card-portfolio">
+        <div class="home card-portfolio__src">
+            <img src="<?php echo $args['preview_picture'];?>" alt="" class="home.card-portfolio__image">
+        </div>
+        <div class="home card-portfolio__content">
+            <div class="home card-portfolio__title h2">
+                <a class="home card-portfolio__link" href="<?php echo $args['link'];?>"><?php echo $args['title'];?></a>
+            </div>
+            <div class="home card-portfolio__description h3 h3--c-200">
+                <?php echo $args['description'];?>
+            </div>
+        </div>
+    </article>
+<?php endif; ?>
